fab690a197f6bdb1fb973fc5e07b9da2af730e7a
Karena Chow <kychow@fb.com>
2018-07-11 14:11:57 -0700

    STARTED:  - (1) uploaded logo / image resource files, colors, and fonts

--- app/src/main/java/comkychow/github/parsestagram/MainActivity.java
+++ app/src/main/java/comkychow/github/parsestagram/MainActivity.java
@@ -1,13 +1,61 @@
 package comkychow.github.parsestagram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.username_et) EditText usernameInput;
+    @BindView(R.id.password_et) EditText passwordInput;
+    @BindView(R.id.login_btn) Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "Login Successful!");
+                    final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.e("LoginActivity", "Login failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/comkychow/github/parsestagram/ParseApplication.java
+++ app/src/main/java/comkychow/github/parsestagram/ParseApplication.java
@@ -5,14 +5,18 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
+import comkychow.github.parsestagram.model.Post;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ParseApplication extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // register ParseApp as subclass
+        ParseObject.registerSubclass(Post.class);
         // staging debug statement
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
@@ -23,45 +27,13 @@ public class ParseApplication extends Application {
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         builder.networkInterceptors().add(httpLoggingInterceptor);
 
-        // set applicationId, server based on Heroku settings values
-        // clientKey only needed if explicity configured
+        // set applicationId & server based on Heroku settings
+        // clientKey only needed if explicitly configured
         // network interceptors must be added with Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("kychow")
                 .clientKey(null)
                 .clientBuilder(builder)
                 .server("http://kychow-fbu-instagram.herokuapp.com/parse").build());
-
-        // test object creation
-        ParseObject testObject = new ParseObject("testObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-
     }
-/*
-    // creates new user in Parse app, checks for both unique username and email
-    public void createNewUser() {
-        // Create the ParseUser
-        ParseUser user = new ParseUser();
-        // Set core properties
-        user.setUsername("joestevens");
-        user.setPassword("secret123");
-        user.setEmail("email@example.com");
-        // Set custom properties
-        user.put("phone", "650-253-0000");
-        // Invoke signUpInBackground
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                    Log.e("Signup ParseException: ", e);
-                }
-            }
-        });
-
-    }
-    */
 }
